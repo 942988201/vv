37cfbce538ca840fe2e5a4ad0070802b3546ceb7
wanglongyi <wangly10@yusys.com.cn>
2020-03-27 18:14:13 +0800

    gg

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id='routerBox'>
+      <keep-alive>
+        <transition name="fade">
+          <router-view v-if="$route.meta.keepAlive"></router-view>
+        </transition>
+      </keep-alive>
+      <transition name="fade">
+        <router-view v-if="!$route.meta.keepAlive">
+          <!-- 这里是不被缓存的视图组件！ -->
+        </router-view>
+      </transition>
+    </div>
+
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+<style lang="less">
+  @import '~vux/src/styles/reset';
+  body {
+   // background-color: @bg;
+    line-height: 1.2;
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html,body,#app,#routerBox{
+    height: 100%;
+  }
+  li{
+    list-style: none;
+  }
 </style>
+<script>
+  import store from './store'
+  import {mapMutations} from 'vuex'
+  export default {
+    store,
+    data() {
+      return {
+        isShow: true
+      }
+    },
+    components: {
+    },
+    methods: {
+      // ...mapMutations(['set_app_tabbar_indexApp']),
+      fetchDate() {
+        let router = this.$route.path;
+        console.log(router)
+        if (router == '/home' || router == '/application' || router == '/mysettings' || router == '/dist/' || router == '/signatureCode') {
+          this.isShow = true;
+          switch (router) {
+            case '/home':
+              // store.dispatch('set_app_tabbar_indexApp', 0);
+              break;
+            case '/application':
+              // store.dispatch('set_app_tabbar_indexApp', 1);
+              break;
+            case '/mysettings':
+              // store.dispatch('set_app_tabbar_indexApp', 2);
+              break;
+            case '/signatureCode':
+              // store.dispatch('set_app_tabbar_indexApp', 3);
+              break;
+            default:
+              // store.dispatch('set_app_tabbar_indexApp', 0);
+          }
+        } else {
+          // this.isShow = false;
+        }
+      }
+    },
+    watch: {
+      "$route": "fetchDate"
+    }
+  }
+
+</script>
